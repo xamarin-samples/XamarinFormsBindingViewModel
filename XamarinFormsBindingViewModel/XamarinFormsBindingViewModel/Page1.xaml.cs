c35f5982840b9b4c5028b83548aef87167715772
kobake <kobake@users.sourceforge.net>
2016-09-03 19:29:45 +0900

    Button

--- XamarinFormsBindingViewModel/XamarinFormsBindingViewModel/Page1.xaml.cs
+++ XamarinFormsBindingViewModel/XamarinFormsBindingViewModel/Page1.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,17 @@ using Xamarin.Forms;
 
 namespace XamarinFormsBindingViewModel
 {
+	
 	public partial class Page1 : ContentPage
 	{
 		public Page1()
 		{
 			InitializeComponent();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
