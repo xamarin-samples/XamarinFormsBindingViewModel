a2e2b860c40a11f0f7c0c56be4ad5dabb2d1246d
kobake <kobake@users.sourceforge.net>
2016-09-03 19:28:05 +0900

    MyViewModel

---
+++ XamarinFormsBindingViewModel/XamarinFormsBindingViewModel/MyViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinFormsBindingViewModel
+{
+	public class MyViewModel : INotifyPropertyChanged
+	{
+		private string mainText = "World";
+		public string MainText
+		{
+			get
+			{
+				return mainText;
+			}
+			set
+			{
+				mainText = value;
+				OnPropertyChanged("MainText");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged(string name)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(name));
+			}
+		}
+	}
+}
